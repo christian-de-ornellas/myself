dcea133dc8d0a383f143ce6d4253a7156ac28dd1
Christian Possidonio <christian.possidonio@itsector.pt>
2022-05-28 22:09:42 -0300

    config hasky and lint staged

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,20 +1,20 @@
-import { NextPage } from "next"
-import Footer from "../footer"
-import Header from "../header"
-import Profile from "../profile"
+import { NextPage } from 'next'
+import Footer from '../footer'
+import Header from '../header'
+import Profile from '../profile'
 
-interface LayoutProps{
-    children: React.ReactNode
+interface LayoutProps {
+  children: React.ReactNode
 }
 
-export const Layout:NextPage<LayoutProps> = ({children}):JSX.Element =>{
-    return(
-        <div>
-            <Header/>
-            <Profile/>
-            <main>{children}</main>
-            <Footer />
-        </div>
-    )
-} 
+export const Layout: NextPage<LayoutProps> = ({ children }): JSX.Element => {
+  return (
+    <div>
+      <Header />
+      <Profile />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  )
+}
 export default Layout
--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,23 +1,32 @@
-import { NextPage } from "next"
-import Image from "next/image"
+import { NextPage } from 'next'
+import Image from 'next/image'
 import styles from './profile.module.css'
 
 export const Profile: NextPage = (): JSX.Element => {
-    return (
+  return (
     <aside>
-        <div className={styles.container}>
-           <div className={styles.heading}>
-           <h1>Desenvolvedor Web & Mobile</h1>
-            <h3>Sou um <u>desenvolvedor Full-Stack</u>, trabalho com desenvolvimento de sistemas
- desde 2013 e tive a oportunidade de trabalhar com diversas tecnologias, hoje 
-meu foco principal é no <i>JavaScript e todo o seu ecossistema</i> para o 
-desenvolvimento web e mobile.</h3>
-           </div>
-            <div> 
-                <Image src="/profile3.png" alt="Christian Possidonio" className={styles.profile} width={800} height={600}/>
-            </div>
+      <div className={styles.container}>
+        <div className={styles.heading}>
+          <h1>Desenvolvedor Web & Mobile</h1>
+          <h3>
+            Sou um <u>desenvolvedor Full-Stack</u>, trabalho com desenvolvimento
+            de sistemas desde 2013 e tive a oportunidade de trabalhar com
+            diversas tecnologias, hoje meu foco principal é no{' '}
+            <i>JavaScript e todo o seu ecossistema</i> para o desenvolvimento
+            web e mobile.
+          </h3>
         </div>
+        <div>
+          <Image
+            src="/profile3.png"
+            alt="Christian Possidonio"
+            className={styles.profile}
+            width={800}
+            height={600}
+          />
+        </div>
+      </div>
     </aside>
-    )
+  )
 }
 export default Profile
